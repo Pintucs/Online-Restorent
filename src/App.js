e7bcf4df43c4b80f9cd4ecd6b0cf75ce21d90cea
Pintucs <pintucs9695@gmail.com>
2023-01-26 00:10:52 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App(){
     return(
         <div className='App'>
         {
-            <>  
+            <>  <h1>hiiii</h1>
                 <Navbar />
                 <Header />
                 <Products />
