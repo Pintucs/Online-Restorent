2baa4e9947d8061404d7d7fd2dc791109820437d
Pintu kumar <pintucs9695@gmail.com>
2023-05-05 15:13:30 +0530

    next commit

--- src/Components/Timeduration.jsx
+++ src/Components/Timeduration.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 let curDate=new Date();
 curDate=curDate.getHours();
 
+// const convertFrom24To12Format = (time24) => {
+//     const [sHours, minutes] = time24.match(/([0-9]{1,2}):([0-9]{2})/).slice(1);
+//     const period = +sHours < 12 ? 'AM' : 'PM';
+//     const hours = +sHours % 12 || 12;
+// }
+
 let pintu= "";
 
 const cssStyle={};
 
 if(curDate>=1&&curDate<=12){
-    pintu="Morning";
+    pintu="Hii, Morning";
     cssStyle.color="red";
 }
 else if(curDate>12&&curDate<18){
